c98eff72f43f354207e241b299a3b4725aeff337
cym350118448 <cym350118448@gmail.com>
2025-05-07 13:34:44 +0800

    5.7.2025

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -53,28 +53,7 @@ Hearing&nbsp;Protection: The Hearing&nbsp;Protection feature works with AirPods&
         </section>
       </div>
       <div class="footer-links">
-        <h3 class="ac-gf-directory-column-section-title">
-					<span class="ac-gf-directory-column-section-title-text">Shop and Learn</span>
-					<button class="ac-gf-directory-column-section-title-button" disabled="">
-						<span class="ac-gf-directory-column-section-title-text">Shop and Learn</span>
-						<span class="ac-gf-directory-column-section-title-icon" aria-hidden="true"><svg class="footer-icon-svg" width="11" height="6" viewBox="0 0 11 6">
-	<polyline data-footer-icon-shape="" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" fill="none" fill-rule="evenodd" points="10.075 0.675 5.5 5.323 0.925 0.675">
-		<animate data-footer-animate="expand" attributeName="points" values="10.075 0.675 5.5 5.323 0.925 0.675;
-					10.075 3 5.5 3 0.925 3;
-					10.075 5.325 5.5 0.676 0.925 5.325" dur="320ms" begin="indefinite" fill="freeze" keyTimes="0;
-					0.5;
-					1" calcMode="spline" keySplines="0.12, 0, 0.38, 0;
-						0.2, 1, 0.68, 1"></animate>
-		<animate data-footer-animate="collapse" attributeName="points" values="10.075 5.325 5.5 0.676 0.925 5.325;
-					10.075 3 5.5 3 0.925 3;
-					10.075 0.675 5.5 5.323 0.925 0.675" dur="320ms" begin="indefinite" fill="freeze" keyTimes="0;
-					0.5;
-					1" calcMode="spline" keySplines="0.2, 0, 0.68, 0;
-						0.2, 1, 0.68, 1"></animate>
-	</polyline>
-</svg></span>
-					</button>
-				</h3>
+        <a href="#">Shop and Learn</a>
         <a href="#">Apple Store</a>
         <a href="#">For Business</a>
         <a href="#">Apple Values</a>
